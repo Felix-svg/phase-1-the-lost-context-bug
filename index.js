7531e22a3c56f9f7a636c52e5d79f1031e4c8e9f
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-01-15 16:44:49 +0300

    Updated script

--- index.js
+++ index.js
@@ -10,3 +10,91 @@ const messageConfig = {
       "Loki"
   ]
 };
+
+//SOLUTION 1a
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${this.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   }, this);
+// };
+
+// printCard.call(messageConfig);
+
+/*
+Happy Birthday, Odin One-Eye!
+From Asgard to Nifelheim, you're the best all-father ever.
+
+Love,
+Admiration, respect, and love, Thor
+Your son, Loki
+*/
+
+//SOLUTION 1b
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+//   const contextBoundForEachExpr = function (signatory) {
+//     const message = `${this.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   }.bind(this);
+
+//   this.signatories.forEach(contextBoundForEachExpr);
+// };
+
+// printCard.call(messageConfig);
+/*
+Happy Birthday, Odin One-Eye!
+From Asgard to Nifelheim, you're the best all-father ever.
+
+Love,
+Admiration, respect, and love, Thor
+Your son, Loki
+*/
+
+//SOLUTION 2
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   const outerContext = this;
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${outerContext.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   });
+// };
+
+// printCard.call(messageConfig);
+/*
+Happy Birthday, Odin One-Eye!
+From Asgard to Nifelheim, you're the best all-father ever.
+
+Love,
+Admiration, respect, and love, Thor
+Your son, Loki
+*/
+
+//SOLUTION 3a
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+  // Wow! Elegant! And notice the arrow function's `this` is the same `this`
+  // that printCard has; specifically, the `thisArg` that was passed to it
+  this.signatories.forEach((signatory) =>
+    console.log(`${this.closing[signatory]}, ${signatory}`)
+  );
+};
+
+printCard.call(messageConfig);
+/* OUTPUT:
+Happy Birthday, Odin One-Eye!
+From Asgard to Nifelheim, you're the best all-father ever.
+
+Love,
+Admiration, respect, and love, Thor
+Your son, Loki
+*/
